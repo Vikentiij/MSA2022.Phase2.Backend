5cac76fc35b3997a395962fd9b1e789e81ecc3da
Viktoriia Protopopova <vikentij.dean@gmail.com>
2022-08-09 22:47:30 +1200

    Create API skeleton

--- Controllers/CatsController.cs
+++ Controllers/CatsController.cs
@@ -52,6 +52,7 @@ namespace MSA2022.Phase2.Backend.Controllers
         /// <summary>
         /// Returns a random cat picture by tag
         /// </summary>
+        /// <param name="tag">The tag for the cat picture to return (required)</param>
         /// <returns>A JSON object with cat picture URL</returns>
         [HttpGet]
         [ProducesResponseType(200)]
@@ -65,6 +66,96 @@ namespace MSA2022.Phase2.Backend.Controllers
 
             var catPicture = await response.Content.ReadFromJsonAsync<CatPicture>();
             return Ok(catPicture);
+        }
+
+        /// <summary>
+        /// Save cat tag
+        /// </summary>
+        /// <returns>A 202 Accepted response</returns>
+        [HttpPost]
+        [ProducesResponseType(202)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> SaveTag(string tag)
+        {
+            if (tag == "") return BadRequest("Tag cannot be empty");
+
+            // TODO: check if tag already exists
+            // if tag found return BadRequest($"Tag {tag} already exists");
+
+            // Fetching the list of all tags that exist in cataas
+            var response = await _client.GetAsync("/api/tags");
+            var allTags = await response.Content.ReadFromJsonAsync<List<string>>();
+
+            if (allTags == null || allTags.Count == 0) return Problem($"Unable to fetch tags from {_client.BaseAddress}");
+
+            if (!allTags.Contains(tag)) {
+                var exampleValidTags = new List<string>();
+                var random = new Random();
+                for (int i = 0; i < 3; i++)
+                {
+                    exampleValidTags.Add(allTags[random.Next(allTags.Count)]);
+                }
+                return BadRequest($"There are no cat pictures for this tag. Try such tags as {string.Join(", ", exampleValidTags)}");
+            }
+
+            return Accepted();
+        }
+
+        /// <summary>
+        /// Update cat tag
+        /// </summary>
+        /// <returns>A 202 Accepted response</returns>
+        [HttpPut]
+        [ProducesResponseType(202)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> UpdateTag(string oldTag, string newTag)
+        {
+            if (oldTag == "" || newTag == "") return BadRequest("Tag cannot be empty");
+
+            // TODO: check if oldTag and newTag exists
+            // if oldTag not found return NotFound($"Cannot update tag {oldTag} because it is not found");
+            // if newTag found return BadRequest($"Tag {newTag} already exists");
+
+            // Fetching the list of all tags that exist in cataas
+            var response = await _client.GetAsync("/api/tags");
+            var allTags = await response.Content.ReadFromJsonAsync<List<string>>();
+
+            if (allTags == null || allTags.Count == 0) return Problem($"Unable to fetch tags from {_client.BaseAddress}");
+
+            if (!allTags.Contains(newTag))
+            {
+                var exampleValidTags = new List<string>();
+                var random = new Random();
+                for (int i = 0; i < 3; i++)
+                {
+                    exampleValidTags.Add(allTags[random.Next(allTags.Count)]);
+                }
+                return BadRequest($"There are no cat pictures for the new tag. Try such tags as {string.Join(", ", exampleValidTags)}");
+            }
+
+            // TODO: repace oldTag with newTag 
+
+            return Accepted();
+        }
+
+        /// <summary>
+        /// Delete cat tag
+        /// </summary>
+        /// <returns>A 204 No Content Response</returns>
+        [HttpDelete]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> DeleteTag(string tag)
+        {
+            if (tag == "") return BadRequest("Tag cannot be empty");
+
+            // TODO: check if tag does not exist
+            // if tag not found return NotFound($"Tag {tag} not found");
+
+            // TODO: delete tag
+
+            return NoContent();
         }
     }
 }
